01550601fe60f0fcb567de192b0a06a77563ec27
korayk <mkoraykocakaya@gmail.com>
2022-04-16 09:33:36 +0300

    Linux control test

--- account/src/main/java/com/kk/account/AccountApplication.java
+++ account/src/main/java/com/kk/account/AccountApplication.java
@@ -1,5 +1,6 @@
 package com.kk.account;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
